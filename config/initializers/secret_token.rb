510156222696422b0a9c73cfa0409f72a669ccac
ka <ka.kaosf@gmail.com>
2013-07-11 16:04:49 +0900

    init
    execute "rails new api-provider-experiment"
    
    versions
    * ruby: 2.0.0-p247
    * rails: 4.0.0

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ApiProviderExperiment::Application.config.secret_key_base = 'b335d1d0068f95db27b34d72223f3c6bffd04e62322238329ba544ff23117a1601be2d6193065acdd5abd387435bfc6c1f640380316814c6cb18b82dc0a58d42'
